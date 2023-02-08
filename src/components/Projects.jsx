b00e5c371799eef0ac83d0a76427cbdc38521c09
João Chocron <schocs13@gmail.com>
2023-02-08 17:38:06 -0300

    Adição momentânea de asset, e adição de projetoitem para visualização

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import ProjectItem from './ProjectItem';
+import java from '../assets/java-svgrepo-com.svg'
 
 const Projects = () => {
   return (
-    <div id='Projetos' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16 sm:mt-[70%] sm:h-screen'>
-        <h1 className='text-gray-900 sm:text-4xl text-2xl font-bold font-montserrat text-center'>Projetos</h1>
-      
+    <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16 sm:mt-[750px] md:mt-[1000px] sm:h-screen'>
+        <h1 className='text-gray-900 sm:text-4xl text-2xl font-bold font-montserrat text-center mb-10'>Projetos</h1>
+        <ProjectItem image={java} titulo={'Olá'} descricao={'Uma descricao'} stack={'Uma stack'}/>
     </div>
   )
 }
