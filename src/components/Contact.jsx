8b7baa82a058809b755d5407e87ac84aad6a2e0b
João Chocron <schocs13@gmail.com>
2023-02-10 11:37:57 -0300

    Criação e estilização básica de Contato

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div id='contact' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16 sm:mt-[750px] md:mt-[800px] sm:h-screen'>
+        <h1 className='text-gray-900 sm:text-4xl text-2xl font-bold font-montserrat text-center mb-[25%]'>Contato</h1>
+    </div>
+  )
+}
+
+export default Contact
