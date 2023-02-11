1393e9551f317b3a7d966c564bc463f3480c5adf
João Chocron <schocs13@gmail.com>
2023-02-11 11:22:27 -0300

    Edição da margem do título Contato

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div id='contact' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16 sm:mt-[750px] md:mt-[800px] sm:h-screen'>
-        <h1 className='text-gray-900 sm:text-4xl text-2xl font-bold font-montserrat text-center mb-[25%]'>Contato</h1>
+        <h1 className='text-gray-900 sm:text-4xl text-2xl font-bold font-montserrat text-center mb-[10%]'>Contato</h1>
         <form action="" method='POST' encType='multipart/form-data'>
             <div>
                 <div>
