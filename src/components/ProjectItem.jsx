1d8095c4a5b298d5ca3542abdc0fe5e1caa6177d
João Chocron <schocs13@gmail.com>
2023-02-13 12:23:53 -0300

    Adição e teste de features

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const ProjectItem = ( { image, alt, titulo, descricao, stack, link } ) => {
   return (
-    <a href='#projects' className='relative w-[90%] m-auto sm:mb-[100px] h-[250px] sm:h-[40%] items-center group hover:mb-[64px] hover:sm:mb-[15%] flex flex-col justify-center hover:sm:flex-row hover:justify-between hover:scale-110'>
-        <div className='relative w-[75%] h-[50%] group-hover:h-[40%] group-hover:sm:h-[90%] sm:w-[50%] group-hover:sm:w-[45%] sm:h-[120%] m-auto bg-slate-400 rounded-lg duration-200 hover:cursor-pointer'>
+    <a href='#projects' className='relative w-[90%] m-auto sm:mb-[100px] h-[250px] sm:h-[40%] items-center group hover:mb-[64px] hover:sm:mb-[15%] flex flex-col justify-center hover:sm:flex-row hover:justify-between hover:scale-125'>
+        <div className='relative w-[75%] h-[50%] group-hover:h-[40%] group-hover:sm:h-[90%] sm:w-[50%] group-hover:sm:w-[50%] group-hover:lg:ml-[25px] sm:h-[120%] m-auto bg-slate-400 rounded-lg duration-200 hover:cursor-pointer'>
             <img src={image} alt={alt} className='rounded-lg object-fill h-full w-full' />
-            <div className='absolute mt-[-1] invisible group-hover:visible w-full h-[15%] m-auto bg-slate-400 rounded-lg p-1'>
+            <div className='absolute mt-[-1] invisible group-hover:visible w-full h-[15%] m-auto bg-slate-400 rounded-lg p-1 group-hover:md:mt-[-21px] group-hover:lg:mt-0'>
                 <p className='font-normal font-montserrat text-slate-700 text-xs sm:text-sm sm:a[font-size:14px] text-center'>Clique para detalhes</p>
             </div>
         </div>
