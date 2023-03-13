f9cc9cb99d988b214f63243b2fa710ec29f03890
João Chocron <schocs13@gmail.com>
2023-03-13 16:42:54 -0300

    Alterações no estilo: adição de seta, há problemas de margem quando se adiciona a seta no elemento inicial

--- src/components/ItemProjeto/ItemProjeto.jsx
+++ src/components/ItemProjeto/ItemProjeto.jsx
@@ -1,5 +1,5 @@
-import classes from './item.module.css'
-
+import classes from './item.module.css';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai'
 import React, { useState, useEffect, useReducer } from 'react';
 
 const ItemProjeto = ( { image, alt, titulo, descricao, stack, link } ) => {
@@ -10,18 +10,35 @@ const ItemProjeto = ( { image, alt, titulo, descricao, stack, link } ) => {
     setIsHidden(!isHidden);
   }
 
+  const [seta, setSeta] = useState(true);
+
+  const showSeta = () => {
+    setSeta(!seta)
+  }
+
+  const [setaFinal, setSetaFinal] = useState(true);
+
+  const showSetaFinal = () => {
+    setSetaFinal(!setaFinal)
+  }
+
   return (
-    <div href='#project' className='item-projeto' onClick={showHidden}>
-      <div className={!isHidden ? classes.hidden : classes.itemInicio}>
+    <div href='#project' className='item-projeto' >
+      <div className={!isHidden ? classes.hidden : classes.itemInicio} onMouseEnter={showSeta} onMouseLeave={showSeta}>
         <div className={classes.imgItem}></div>
-        <div className={classes.sideItem}></div>
+        <div className={seta ? classes.seta : classes.sideItem} onClick={showHidden}>
+          <AiOutlineArrowRight style={{width: '100%', height: '100%'}} /> 
+        </div>
       </div>
-      <div className={isHidden ? classes.hidden : classes.itemFinal}>
+      <div className={isHidden ? classes.hidden : classes.itemFinal} onMouseEnter={showSetaFinal} onMouseLeave={showSetaFinal}>
       <div className={classes.tituloItem}>
           <h3>inicio</h3>
         </div>
         <div className={classes.bodyItem}>
-          
+          <div className={setaFinal ? classes.setaFinal : classes.sideItemFinal} onClick={showHidden}>
+            <AiOutlineArrowLeft style={{width: '100%', height: '100%'}} />
+          </div>
+          <div className={classes.mainBody}><h1>Olá</h1></div>
         </div>
         <div className={classes.footItem}>
           <a href={link} target={'_blank'}>
