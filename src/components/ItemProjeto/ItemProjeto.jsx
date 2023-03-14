ca8b6dc0f134aecb09b0e9a7d972c6b56b8a6306
João Chocron <schocs13@gmail.com>
2023-03-14 15:40:06 -0300

    Modificações finais do Item projeto da parte de mídias superiores a 640px. Falta a modificação para responsividade do site

--- src/components/ItemProjeto/ItemProjeto.jsx
+++ src/components/ItemProjeto/ItemProjeto.jsx
@@ -25,26 +25,29 @@ const ItemProjeto = ( { image, alt, titulo, descricao, stack, link } ) => {
   return (
     <div href='#project' className='item-projeto' >
       <div className={!isHidden ? classes.hidden : classes.itemInicio} onMouseEnter={showSeta} onMouseLeave={showSeta}>
-        <div className={classes.imgItem}></div>
+        <div className={classes.imgItem}>
+          <img src={image} alt={alt} className={classes.imgInicio} />
+        </div>
         <div className={seta ? classes.seta : classes.sideItem} onClick={showHidden}>
-          <AiOutlineArrowRight style={{width: '100%', height: '100%'}} /> 
+          <AiOutlineArrowRight style={{width: '40px', height: '40px'}} /> 
         </div>
       </div>
       <div className={isHidden ? classes.hidden : classes.itemFinal} onMouseEnter={showSetaFinal} onMouseLeave={showSetaFinal}>
       <div className={classes.tituloItem}>
-          <h3>inicio</h3>
+          <h3 style={{fontWeight: 'bold'}}>{titulo}</h3>
         </div>
         <div className={classes.bodyItem}>
           <div className={setaFinal ? classes.setaFinal : classes.sideItemFinal} onClick={showHidden}>
-            <AiOutlineArrowLeft style={{width: '100%', height: '100%'}} />
+            <AiOutlineArrowLeft style={{width: '40px', height: '40px'}} />
           </div>
-          <div className={classes.mainBody}><h1>Olá</h1></div>
+          <div className={classes.mainBody}><p style={{fontSize: '18px'}}>{descricao}</p></div>
         </div>
         <div className={classes.footItem}>
           <a href={link} target={'_blank'}>
-            <h3>mudou</h3>
+            <p className={classes.linkItem}>Clique aqui para ter acesso ao código</p>
           </a>
         </div>
+        <div className={classes.stackItem}><p>Stack: {stack}</p></div>
       </div>
     </div>
   )
