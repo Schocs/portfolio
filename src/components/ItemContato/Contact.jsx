bc29c1d709c0b17cb228d3edf87c7634082eaeb2
João Chocron <schocs13@gmail.com>
2023-05-16 17:59:16 -0300

    Adição de função para popup informando sobre o status do envio da mensagem. Modificação de estilo no botão de envio. Falta responsividade para pequenas telas

--- src/components/ItemContato/Contact.jsx
+++ src/components/ItemContato/Contact.jsx
@@ -1,7 +1,8 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import classes from './item.module.css';
 import emailjs from '@emailjs/browser';
-import { SiGmail, SiGithub, SiLinkedin } from 'react-icons/si'
+import { SiGmail, SiGithub, SiLinkedin } from 'react-icons/si';
+import PopUp from '../PopUp/PopUp';
 
 const Contact = () => {
 
@@ -13,9 +14,23 @@ const Contact = () => {
         emailjs.sendForm('contact_service', 'contact_form', form.current, 'mDCUJXRTNM9ngGtrl')
         .then((result) => {
             console.log(result.text);
+            showPop();
         }, (error) => {
             console.log(error.text);
+            showError();
         });
+    }
+
+    const [popTrigger, setPopTrigger] = useState(false)
+
+    const showPop = () => {
+        setPopTrigger(!popTrigger);
+    }
+
+    const [errorTrigger, setErrorTrigger] = useState(false)
+
+    const showError = () => {
+        setErrorTrigger(!errorTrigger);
     }
 
     return (
@@ -37,25 +52,27 @@ const Contact = () => {
                 <div className={classes.mainForm}>
                     <div className={classes.itemForm}>
                         <label htmlFor="" className={classes.labelForm}>Nome:</label>
-                        <input type="text" name='user_name' className={classes.inputForm}/>
+                        <input type="text" name='user_name' className={classes.inputForm} required/>
                     </div>
                     <div className={classes.itemForm}>
                         <label className={classes.labelForm}>Email:</label>
-                        <input type="email" name="user_email" id="" className={classes.inputForm}/>
+                        <input type="email" name="user_email" id="" className={classes.inputForm} required/>
                     </div>
                     <div className={classes.itemForm}>
                         <label className={classes.labelForm}>Assunto:</label>
-                        <input type="text" name="subject" id="" className={classes.inputForm}/>
+                        <input type="text" name="subject" id="" className={classes.inputForm} required/>
                     </div>
                 </div>
                 <div className={classes.msgForm}>
                     <label className={classes.labelForm}>Mensagem:</label>
-                    <textarea rows={20} name='message' className={classes.txtArea}></textarea>
+                    <textarea rows={20} name='message' className={classes.txtArea} required></textarea>
                 </div>
                 <div className={classes.btnDiv}>
-                    <input className={classes.buttonForm} type='' value='Enviar'/> 
+                    <button className={classes.buttonForm}>Enviar</button>
                 </div>
             </form>
+            <PopUp trigger={popTrigger} setTrigger={setPopTrigger} resultado={'Sucesso!'} msg={'Sua mensagem foi enviada com sucesso! Em até 2 dias úteis, responderei através do endereço joaochocron@gmail.com'} check={'Obrigado pela mensagem!'}/>
+            <PopUp trigger={errorTrigger} setTrigger={setErrorTrigger} resultado={'Algo deu errado!'} msg={'Tente novamente mais tarde, ou envie um e-mail diretamente através do endereço joaochocron@gmail.com'} check={'Obrigado pelo interesse!'}/>
         </div>
   )
 }
